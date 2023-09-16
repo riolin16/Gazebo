90197ad8723a4afdbb34ed48368aa79e283540cf
Rio <rio.lin+SYNPULSE@synpulse.com>
2023-09-16 19:06:56 +1000

    Create Team model

--- gazebo/db/models/team_model.py
+++ gazebo/db/models/team_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, inspect, Integer
+from sqlalchemy import Column, inspect, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from gazebo.db.models import Base
 
@@ -6,8 +7,12 @@ from gazebo.db.models import Base
 class Team(Base):
     __tablename__ = 'teams'
 
-    id = Column(Integer, primary_key=True, index=True)
-    users = Column()
+    team_id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    parent_team_id = Column(Integer, ForeignKey('teams.team_id'), nullable=True)
+
+    users = relationship('User', backref='team')
+    child_teams = relationship('Team', remote_side=[team_id], backref='parent_team')
 
     def to_dict(self) -> dict:
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
--- gazebo/db/models/user_model.py
+++ gazebo/db/models/user_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, inspect, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 from gazebo.db.models import Base
 
@@ -8,7 +9,9 @@ class User(Base):
 
     uid = Column(String(50), primary_key=True, index=True)  # also Firebase UID
     username = Column(String, unique=True, nullable=False, index=True)
-    team_id = Column(Integer, index=True)
+
+    team_id = Column(Integer, ForeignKey('teams.team_id'), index=True)
+    team = relationship('Team', back_populates='users')
 
     def to_dict(self) -> dict:
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
